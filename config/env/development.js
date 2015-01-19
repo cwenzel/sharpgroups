bdbb085a7210271908427e366b215fe79ec427be
Cody Wenzel <cody.wenzel@gmail.com>
2015-01-19 07:55:47 -0500

    new favicon, fixed title

--- config/env/development.js
+++ config/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
 		}
 	},
 	app: {
-		title: 'MEAN.JS - Development Environment'
+		title: 'sharpgroups - Fantasy games for fun'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
